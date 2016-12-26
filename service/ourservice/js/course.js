47e14122d62ca2ca89c35321e39166d4a20ceeea
tzh <tanzhihui147@qq.com>
2016-12-26 10:08:37 +0800

    分享页面并且合并代码

--- service/ourservice/js/course.js
+++ service/ourservice/js/course.js
@@ -1,3 +1,8 @@
 $(function () {
-
+    $('#img0').click(function () {
+        $('#targetImg').attr('src','../../../image/protocal1.jpg');
+    });
+    $('#img1').click(function () {
+        $('#targetImg').attr('src','../../../image/protocal2.jpg');
+    })
 });
