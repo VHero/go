a726f77fe7623cb0085fef4fba4cc990364584b1
VHero <a772839689@gmail.com>
2016-12-29 18:23:23 +0800

    comment php change

--- userComments/comments.php
+++ userComments/comments.php
@@ -59,20 +59,45 @@
     <!--评论区-->
     <div class="comArea">
         <div class="container">
-            <div class="panel panel-info panelSty">
-                <div class="panel-heading">
-                    <div>
-                        <img src="https://p0.meituan.net/avatar/__47564542__7122477.jpg@100w_100h_1e_1c" alt="头像" class="img-circle imgSty">
-                        <h3 class="panel-title">小五&nbsp;&nbsp;&nbsp;&nbsp;Penn State U&nbsp;&nbsp;&nbsp;&nbsp;发表于2015-12-15</h3>
-                    </div>
+        <?php
+            include "../function.php";
+                $commentTableName = "comment";
+                $commentArray = array("id" =>"", "imgsrc" =>"","comment"=>"","date"=>"","name"=>"");
+                $commentCurrentPage = 1; //1页
+                $commentPageSize = 20; //10个
+                $commentOrder = "id"; //按照id排序
+                $commentSort = "asc";//desc逆序
+                $commentParamSql = null;
+                $commentList = search($commentTableName, $commentCurrentPage, $commentPageSize, $commentOrder, $commentSort, $commentArray, $commentParamSql);
+                 foreach($commentList as $obj) {
+                echo "<div class='panel panel-info panelSty'>
+                         <div class='panel-heading'>
+                            <div>
+                                <img src=../".$obj['imgsrc']. " alt='头像' class='img-circle imgSty'>
+                                <h3 class='panel-title'>".$obj['name']."&nbsp;&nbsp;&nbsp;&nbsp;发表于".$obj['date']."</h3>
+                            </div>
+                         </div>
+                         <div class='panel-body'>"
+                           .$obj['comment']."
+                         </div>
+                       </div>  
+                     ";
+                    } 
 
-                </div>
-                <div class="panel-body">
-                    我想上一门 Accounting的网课，但是自己找不到合适的。我想找一个是4年本科不是2年社区学员的，最好便宜一点的。
-                    咨询了上课帮，帮我推荐了一门比较适合我现在情况的课程。我就报名代修了这门课，老师很专业，帮我拿到了很高的学分。
-                </div>
-            </div>
-            <div class="panel panel-info panelSty">
+        ?>
+            
+                
+                    
+                        
+                        
+                    
+
+                
+                
+
+                
+            
+ <!--            <div class="panel panel-info panelSty">
                 <div class="panel-heading">
                     <div>
                         <img src="https://p0.meituan.net/avatar/__47564542__7122477.jpg@100w_100h_1e_1c" alt="头像" class="img-circle imgSty">
@@ -119,19 +144,20 @@
                     我想上一门 Accounting的网课，但是自己找不到合适的。我想找一个是4年本科不是2年社区学员的，最好便宜一点的。
                     咨询了上课帮，帮我推荐了一门比较适合我现在情况的课程。我就报名代修了这门课，老师很专业，帮我拿到了很高的学分。
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
     <div class="text-center">
         <ul class="pagination">
-            <li><a href="#">&laquo;</a></li>
+          <li><a href="#">&laquo;</a></li>
             <li class="active"><a href="#">1</a></li>
             <li><a href="#">2</a></li>
             <li><a href="#">3</a></li>
             <li><a href="#">4</a></li>
             <li><a href="#">5</a></li>
-            <li><a href="#">&raquo;</a></li>
+            <li><a href="#">&raquo;</a></li> 
+
         </ul>
     </div>
 
